36d501c5865217a6aa6fc07e47208a4e0799e961
Robin <robin.lewis@anterior.com>
2025-09-09 18:15:06 -0400

    fix: package without any dependencies (#9)
    * fix: package without any dependencies
    
    * test: workspace without dependencies
    
    * test: package without dependencies
    
    * test: local non-workspace dependency

---
+++ tests/single-no-deps/foo.test.js
@@ -0,0 +1,10 @@
+import { suite, test } from "node:test";
+import { strictEqual } from "node:assert/strict";
+
+import { foo } from "./foo.js";
+
+await suite(import.meta.filename, async () => {
+  await test("foo()", async () => {
+    strictEqual(foo(), "foobar");
+  });
+});
