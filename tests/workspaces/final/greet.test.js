8e5445db4bc6b9c518c9d1a34bab79b6c5cba428
Robin <robin.lewis@anterior.com>
2025-09-09 00:36:16 -0400

    test: nested external dependency in workspace

--- tests/workspaces/final/greet.test.js
+++ tests/workspaces/final/greet.test.js
@@ -1,10 +1,20 @@
 import { suite, test } from "node:test";
 import { strictEqual } from "node:assert/strict";
+// Local copy, different version from dependency
+import bencode from "bencode";
 
 import { greet } from "./greet.js";
 
 await suite(import.meta.filename, async () => {
   await test("greet", async () => {
     strictEqual(greet("foo"), "Hello, 3:foo");
+  });
+
+  await test("local dependency", async () => {
+    // Test that this is indeed the old version of bencode by triggering old behavior
+    const enc = bencode.encode;
+    enc._floatConversionDetected = true;
+    const astxt = new TextDecoder("UTF-8").decode(enc(340282366920938463463374607431768211457));
+    strictEqual(astxt, "i0e");
   });
 });
